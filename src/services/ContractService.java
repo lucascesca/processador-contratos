01b403c64f1a6073f71c0c7ab715c7549d15db9f
Lucas Cesca <lucascesca14@gmail.com>
2023-12-16 17:05:36 -0300

    Classes created

---
+++ src/services/ContractService.java
@@ -0,0 +1,8 @@
+package services;
+
+import entities.Contract;
+
+public class ContractService {
+    private OnlinePaymentService onlinePaymentService;
+    public void processContract(Contract contract, int months) {}
+}
